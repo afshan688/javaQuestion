e3617e6b706fe8890c61f235a4d4f28cc7ba0af5
afshan688 <afshanshakeel688@gmail.com>
2024-09-06 10:02:15 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,122 @@
+alert('Error please enter correct password' 
+    
+);
+alert('Welcome to js land...\nHappy codding');
+
+alert('Happy codding\nPrevent this page from creating addional dailogs!');
+
+
+alert('Hello... I can run JS through my web browser console');
+
+// Ch..2 //
+var fullname= 'Afshan';
+var  age= '28';
+var education= 'Certified web and App Development';
+alert(fullname);
+alert(age);
+alert(education);
+
+var food= 'pizza \n pizz \n piz \npi';
+alert(food);
+
+var email= 'afshanshakeel688gmail.com';
+alert(email);
+
+var book= 'A Smater way to learn JAVA Script';
+alert('I am trying to learn from the' +book) ;
+
+document.write('yeh! Ican write html content trough Java Script. </br>') ;
+
+//CH ,3 //
+
+var age= ' I am 28 years old' ;
+alert(age);
+
+var visit= 'You have visit this site 14 times';
+alert(visit);
+
+var birthyear= 1994 ;
+document.write('My birth year is' + birthyear +'</br>');
+
+var name= 'John  Doe';
+var product= 'T  shirts';
+var quantity= '5';
+document.write(name   +  'orderd'   +  quantity +  product  +  'on xyz clothing store .</br> </br>');
+
+  //Ch...4//
+
+  var name='ab' , age='15' , num= '2';
+
+  document.write('LEGAL VARIABLE</br> username</br> _age </br> totalAmount</br> count2</br> price_per_item</br></br>');
+  document.write('IILEGAL VARIABLE</br> 2count </br> class </br> total-amount </br> @name </br> first  name </br></br>')
+
+ document.write('<b> Rules for naming JS Variables</b><br><br>');
+ document.write('Variables that also contain , numbers ,$ ,_ and For example: $my_firstVariable <br>');
+ document.write('Variable must begin with a letter,$ or _ . For example: $name,_ name or name <br> ');
+ document.write('Variables name are case sensitive <br>');
+ document.write('Variable names should not be JS keywords <br>');
+
+        //HOME WORK//
+ 
+    //Ch....1 //
+   var firstname='any'
+  alert(firstname);
+  alert(1234556);
+  alert('password');
+  alert('You are learning JavaScript');
+
+    // Ch...2 //
+
+var userName='abc';
+var teamname= 'karachi king';
+alert(teamname);
+var bestman ='Imran Khan';
+
+
+  //Ch....3 //
+
+var caseQty=  2;
+var caseQty =  144;
+var num = '9';
+
+var x = 3 , y = 4 ;
+var sum =x + y ;
+
+var mearchTotal = 100;
+var shippingcharge = 10;
+var orderTotal =mearchTotal+ shippingcharge ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
